fbdd1299d7340e4e09db34fad1a1fd6535a581fd
LauraMolano <lauraisamolano@unicauca.edu.co>
2025-10-20 00:17:40 -0500

    🚧 Avance backend inicio

--- backend/coordinator-service/src/main/java/co/edu/unicauca/coordinatorservice/controller/CoordinadorController.java
+++ backend/coordinator-service/src/main/java/co/edu/unicauca/coordinatorservice/controller/CoordinadorController.java
@@ -1,4 +1,26 @@
 package co.edu.unicauca.coordinatorservice.controller;
 
+import co.edu.unicauca.coordinatorservice.entity.Coordinador;
+import co.edu.unicauca.coordinatorservice.infra.CoordinadorInfoDTO;
+import co.edu.unicauca.coordinatorservice.repository.CoordinadorRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/coordinador")
 public class CoordinadorController {
+    private final CoordinadorRepository coordinadorRepository;
+
+    public CoordinadorController(CoordinadorRepository coordinadorRepository) {
+        this.coordinadorRepository = coordinadorRepository;
+    }
+
+    @GetMapping("/{id}/info")
+    public CoordinadorInfoDTO getCoordinadorInfo(@PathVariable Long id) {
+        Coordinador c = coordinadorRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("No encontrado"));
+        return new CoordinadorInfoDTO(c.getNombres() + " " + c.getApellidos(), c.getPrograma().toString());
+    }
 }
---
+++ backend/coordinator-service/src/main/java/co/edu/unicauca/coordinatorservice/infra/CoordinadorInfoDTO.java
@@ -0,0 +1,5 @@
+package co.edu.unicauca.coordinatorservice.infra;
+
+public record CoordinadorInfoDTO(String nombreCompleto, String programa) {
+
+}
