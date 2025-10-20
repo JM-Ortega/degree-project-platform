4b608b05e4e5ccf3d62c83d9ee366c7a60afba84
LauraMolano <lauraisamolano@unicauca.edu.co>
2025-10-19 21:41:11 -0500

    🗃️ Entidad Formato A

--- backend/coordinator-service/src/main/java/co/edu/unicauca/coordinatorservice/entity/FormatoA.java
+++ backend/coordinator-service/src/main/java/co/edu/unicauca/coordinatorservice/entity/FormatoA.java
@@ -17,4 +17,74 @@ public class FormatoA {
     private LocalDate fechaSubida;
     private byte[] blob;
     private EstadoFormatoA estado;
+
+    public FormatoA(Long id, Long proyectoId, int nroVersion, String nombre, LocalDate fechaSubida, byte[] blob, EstadoFormatoA estado) {
+        this.id = id;
+        this.proyectoId = proyectoId;
+        this.nroVersion = nroVersion;
+        this.nombre = nombre;
+        this.fechaSubida = fechaSubida;
+        this.blob = blob;
+        this.estado = estado;
+    }
+
+    public FormatoA() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getProyectoId() {
+        return proyectoId;
+    }
+
+    public void setProyectoId(Long proyectoId) {
+        this.proyectoId = proyectoId;
+    }
+
+    public int getNroVersion() {
+        return nroVersion;
+    }
+
+    public void setNroVersion(int nroVersion) {
+        this.nroVersion = nroVersion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaSubida() {
+        return fechaSubida;
+    }
+
+    public void setFechaSubida(LocalDate fechaSubida) {
+        this.fechaSubida = fechaSubida;
+    }
+
+    public EstadoFormatoA getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoFormatoA estado) {
+        this.estado = estado;
+    }
+
+    public byte[] getBlob() {
+        return blob;
+    }
+
+    public void setBlob(byte[] blob) {
+        this.blob = blob;
+    }
 }
