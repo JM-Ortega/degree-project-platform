5fd4b2b0c27407d847ca9a08b5557835085d3480
Juan Ortega <jmo.narvaez@gmail.com>
2025-11-03 06:17:17 -0500

    🛡️ types: actualizar enums de departamento programa y rol en frontend

--- frontend/src/main/java/co/edu/unicauca/frontend/entities/enums/Programa.java
+++ frontend/src/main/java/co/edu/unicauca/frontend/entities/enums/Programa.java
@@ -1,8 +1,23 @@
 package co.edu.unicauca.frontend.entities.enums;
 
 public enum Programa {
-    IngenieriaDeSistemas,
-    IngenieriaElectronicaYTelecomunicaciones,
-    AutomaticaIndustrial,
-    TecnologiaEnTelematica
+    INGENIERIA_DE_SISTEMAS("Ingeniería de Sistemas"),
+    INGENIERIA_ELECTRONICA_Y_TELECOMUNICACIONES("Ingeniería Electrónica y Telecomunicaciones"),
+    AUTOMATICA_INDUSTRIAL("Automática Industrial"),
+    TECNOLOGIA_EN_TELEMATICA("Tecnología en Telemática");
+
+    private final String nombreDisplay;
+
+    Programa(String nombreDisplay) {
+        this.nombreDisplay = nombreDisplay;
+    }
+
+    @Override
+    public String toString() {
+        return nombreDisplay;
+    }
+
+    public String getNombreDisplay() {
+        return nombreDisplay;
+    }
 }
