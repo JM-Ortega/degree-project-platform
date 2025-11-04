b0cbfd800f500dd7b4664605bf7a38129910438f
MaryuriFernandez <maryurifernandez437@gmail.com>
2025-11-03 19:54:51 -0500

    ✨feature: se añade lógica para guardar datos de JefeDeDepartamento y Coordinador en los repositorios

--- backend/academic-project-service/src/main/java/co/edu/unicauca/academicprojectservice/Repository/CoordinadorRepository.java
+++ backend/academic-project-service/src/main/java/co/edu/unicauca/academicprojectservice/Repository/CoordinadorRepository.java
@@ -1,4 +1,10 @@
 package co.edu.unicauca.academicprojectservice.Repository;
 
-public interface CoordinadorRepository {
+import co.edu.unicauca.academicprojectservice.Entity.Coordinador;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CoordinadorRepository extends JpaRepository<Coordinador, Long> {
+    Optional<Coordinador> findByCorreo(String correo);
 }
--- backend/academic-project-service/src/main/java/co/edu/unicauca/academicprojectservice/Repository/JefeDeDepartamentoRepository.java
+++ backend/academic-project-service/src/main/java/co/edu/unicauca/academicprojectservice/Repository/JefeDeDepartamentoRepository.java
@@ -1,4 +1,10 @@
 package co.edu.unicauca.academicprojectservice.Repository;
 
-public interface JefeDeDepartamentoRepository {
+import co.edu.unicauca.academicprojectservice.Entity.JefeDeDepartamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface JefeDeDepartamentoRepository extends JpaRepository<JefeDeDepartamento, Long> {
+    Optional<JefeDeDepartamento> findByCorreo(String correo);
 }
